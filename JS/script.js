b2fa0d27146c8a1cf06c87b7fc630894132e6cef
devcharle <charlexavier100@gmail.com>
2022-10-15 17:07:21 -0300

    refatoracao de codigo

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,10 @@
-let aa =  document.getElementById('requests');
-let bb = document.getElementById('newRequest');
-let btn = document.getElementById('btn-novo-pedido');
+let aa = document.getElementById("requests");
+let bb = document.getElementById("newRequest");
+let btn = document.getElementById("btn-novo-pedido");
 
-let tradePage = () =>{
-    aa.setAttribute('hidden', 'true')
-    bb.removeAttribute('hidden')
+let tradePage = () => {
+  aa.setAttribute("hidden", "true");
+  bb.removeAttribute("hidden");
 };
 
-btn.addEventListener('click',tradePage)
+btn.addEventListener("click", tradePage);
