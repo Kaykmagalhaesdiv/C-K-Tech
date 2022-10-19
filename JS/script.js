ccbbc4bcc1c2e1330f96a9792d3120527db09924
Kayk magalhães <leesunga0@gmail.com>
2022-10-18 21:42:54 -0300

    Adicionado mudança na table, total do pedio e salvar pedido!

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,7 @@ let secondPage= document.getElementById("newRequest");
 let btnNewRequest = document.getElementById("btn-novo-pedido");
 let btnFind = document.getElementById('btn-find')
 let btnAddProduct = document.getElementById('btn-add');
+let btnSave = document.getElementById('btn-save-id')
 
 let inputQuantidade = document.getElementById("amount-product");
 let inputProduct = document.getElementById("name-product");
@@ -11,8 +12,10 @@ let inputPreco = document.getElementById("price-product");
 let inputFindProduct = document.getElementById('codeProduct')
 
 let tableBody = document.getElementById('table-body')
+let tableBodyStatus = document.getElementById('table-body-result');
 
 let imgBody = document.getElementById('img-hidden');
+let imgFirstPage = document.getElementById('img-hidden-first')
 
 
 let obj = [
@@ -28,6 +31,7 @@ let obj = [
   },
 ];
 
+let arrProductClient = [];
 
 let tradePage = () => {
   firstPage.setAttribute("hidden", "true");
@@ -51,7 +55,7 @@ let findProduct = () =>{
   });
 
   if(findObj.length == 0 || inputQuantidade.value == '' || inputQuantidade.value <= 0 || inputFindProduct.value == ''){
-    alert('Por favor, verifique o código do produto ou Quantidade estão corretos.')
+    alert('Por favor, verifique o código do produto ou Quantidade estão corretos.');
   }else{
     inputProduct.value = findObj[0].produto;
     inputPreco.value = arrSum[0].toFixed(2);
@@ -69,8 +73,73 @@ let productTable = () =>{
                           <td>${inputQuantidade.value}</td>
                           <td>R$ ${inputPreco.value}</td>
                         </tr>`
+  let type = document.querySelector('input[name="option"]:checked').value                    
+  arrProductClient.push({
+  codigo: inputFindProduct.value,
+  produto: inputProduct.value,
+  quantidade: inputQuantidade.value,
+  preco: parseInt(inputPreco.value),
+  tipo: type
+  });
+
+  console.log(arrProductClient)
+
+  inputFindProduct.value = ''
+  inputProduct.value = ''
+  inputQuantidade.value = ''
+  inputPreco.value =  ''
+
+
+  let filterPrice = arrProductClient.map((valor) =>{
+    return valor.preco
+  })
+
+  let totalSum = filterPrice.reduce((total,next) =>{
+    return total += next;
+  })
+
+  let resultPrice = document.getElementById('result')
+  resultPrice.removeAttribute('hidden')
+  resultPrice.innerHTML = `<h4>
+                            Total do pedido: <strong>R$ ${totalSum}</strong:
+                          </h4>`;
+
+  btnSave.removeAttribute('class', 'btn-save-inactive');
+  btnSave.setAttribute('class','btn-save btn-config btn-save-active')
+  
+
+  if(inputFindProduct.value == '' || inputQuantidade.value  == ''){
+    btnAddProduct.removeAttribute('class', 'btn-active');
+    btnAddProduct.setAttribute('class', 'btn-inactive btn-save-product btn-config mt-3')
+    btnAddProduct.setAttribute('disabled','true');
+  }
+  return false
+  // arrProductClient.forEach((element) => {
+
+  // })
+};
+
+let saveProduct = () =>{
+  secondPage.setAttribute('hidden','true')
+  firstPage.removeAttribute('hidden');
+
+  imgFirstPage.setAttribute('hidden','true')
+
+arrProductClient.forEach((element) =>{
+  tableBodyStatus.innerHTML += `<tr>
+                                <td> <input type="radio"/> 88888
+                                </td>
+                                <td>${element.quantidade} - ${element.produto}</td>
+                                <td>${element.tipo}</td>
+                                <td>R$ ${element.preco.toFixed(2)}</td>
+                              </tr>`
+})
+  
 }
+
+
 
 btnNewRequest.addEventListener("click", tradePage);
 btnFind.addEventListener("click", findProduct);
 btnAddProduct.addEventListener("click", productTable)
+btnSave.addEventListener('click',saveProduct)
